bd344813557510591a1dd65edd93582a0e2ac57e
azams <azams@LAPTOP-7VNCPRLE>
2021-05-25 21:18:21 +0530

    Facebook page open

---
+++ src/main/java/seleniumlearning/SeleniumLearning.java
@@ -0,0 +1,21 @@
+package seleniumlearning;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class SeleniumLearning {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.facebook.com/");
+		driver.manage().window().maximize();
+		Thread.sleep(3000);
+		driver.close();
+
+	}
+
+}
